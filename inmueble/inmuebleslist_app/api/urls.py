9bb3360b0ac1fd9f5e8262bd9b49512e5d93fe98
Nuevo nombre de usuario <nuevo@email.com>
2023-08-27 12:04:35 -0500

    curso django 1.1

--- inmueble/inmuebleslist_app/api/urls.py
+++ inmueble/inmuebleslist_app/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from inmuebleslist_app.api.views import inmueble_list,inmueble_detalle
+# from inmuebleslist_app.api.views import inmueble_list,inmueble_detalle
+from inmuebleslist_app.api.views import InmuebleListAV,InmuebleDetalleAV
 
 urlpatterns = [
-    path('list/',inmueble_list,name='inmueble_list'),
-    path('<int:pk>',inmueble_detalle,name='inmueble_detalle'),
+    path('list/',InmuebleListAV.as_view(),name='inmueble_list'),
+    path('<int:pk>',InmuebleDetalleAV.as_view(),name='inmueble_detalle'),
+    
 ]
