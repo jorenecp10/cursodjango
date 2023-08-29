7ccc6a9743ce56235167499bdc3d92bae65088f6
Jose Rene Correa Padilla <jose.correa@constructorabolivar.com>
2023-08-29 15:55:06 -0500

    curso 1 django hotel 29/08/2023

--- inmueble/inmuebleslist_app/api/serializers.py
+++ inmueble/inmuebleslist_app/api/serializers.py
@@ -1,41 +1,64 @@
 from rest_framework import serializers
 from inmuebleslist_app.models import Inmueble
 
-def column_longitud(value):
-    if len(value) < 2:
-        raise serializers.ValidationError("El valor es demasiado corto")
-
-class InmuebleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    direccion = serializers.CharField(validators=[column_longitud])
-    pais=serializers.CharField(validators=[column_longitud])
-    descripcion = serializers.CharField()
-    imagen = serializers.CharField()
-    activate=serializers.BooleanField()
-    
-    def create(self, validated_data):
-        return Inmueble.objects.create(**validated_data)
-    
-    def update(self,instance, validated_data):
-        instance.direccion = validated_data.get('direccion',instance.direccion)
-        instance.pais = validated_data.get('pais',instance.pais)
-        instance.descripcion = validated_data.get('descripcion',instance.descripcion)
-        instance.imagen = validated_data.get('imagen',instance.imagen)
-        instance.activate = validated_data.get('activate',instance.activate)
-        instance.save()
-        return instance
-    
+class InmuebleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inmueble
+        fields = '__all__'
+        # fields = ['id','pais','activate','imagen']
+        # exclude =['id']
+        
     def validate(self, data):
         if data['direccion']==data['pais']:
-            raise serializers.ValidationError("La direccion y el pais deben ser diferentes")
+             raise serializers.ValidationError("La direccion y el pais deben ser diferentes")
         else:
-            return data
+             return data
         
     def validate_imagen(self, data):
         if len(data)<2:
             raise serializers.ValidationError("La url de la imagen es muy corta")
         else:   
             return data
+
+    
+
+
+
+# def column_longitud(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError("El valor es demasiado corto")
+
+# class InmuebleSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     direccion = serializers.CharField(validators=[column_longitud])
+#     pais=serializers.CharField(validators=[column_longitud])
+#     descripcion = serializers.CharField()
+#     imagen = serializers.CharField()
+#     activate=serializers.BooleanField()
+    
+#     def create(self, validated_data):
+#         return Inmueble.objects.create(**validated_data)
+    
+#     def update(self,instance, validated_data):
+#         instance.direccion = validated_data.get('direccion',instance.direccion)
+#         instance.pais = validated_data.get('pais',instance.pais)
+#         instance.descripcion = validated_data.get('descripcion',instance.descripcion)
+#         instance.imagen = validated_data.get('imagen',instance.imagen)
+#         instance.activate = validated_data.get('activate',instance.activate)
+#         instance.save()
+#         return instance
+    
+#     def validate(self, data):
+#         if data['direccion']==data['pais']:
+#             raise serializers.ValidationError("La direccion y el pais deben ser diferentes")
+#         else:
+#             return data
+        
+#     def validate_imagen(self, data):
+#         if len(data)<2:
+#             raise serializers.ValidationError("La url de la imagen es muy corta")
+#         else:   
+#             return data
         
         
     
