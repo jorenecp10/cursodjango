3f1860efad34d313b5e96fdfb9ff54cd67b7efd3
Jose Rene Correa Padilla <jose.correa@constructorabolivar.com>
2023-08-30 14:56:46 -0500

    avance en el hotel 30/08/2023

--- inmueble/inmuebleslist_app/models.py
+++ inmueble/inmuebleslist_app/models.py
@@ -2,18 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Edificacion(models.Model):
-    direccion = models.CharField(max_length=250)
-    pais=models.CharField(max_length=150)
-    descripcion=models.CharField(max_length=500)
-    imagen=models.CharField(max_length=900)
-    activate=models.BooleanField(default=True)
-    creates=models.DateTimeField(auto_now_add=True)
-    
-    
-    def __str__(self):
-        return self.direccion
-    
 class Empresa(models.Model):
     nombre=models.CharField(max_length=250)
     website=models.URLField(max_length=250)
@@ -21,5 +9,21 @@ class Empresa(models.Model):
     
     def __str__(self):
         return self.nombre
+
+class Edificacion(models.Model):
+    direccion = models.CharField(max_length=250)
+    pais=models.CharField(max_length=150)
+    descripcion=models.CharField(max_length=500)
+    imagen=models.CharField(max_length=900)
+    activate=models.BooleanField(default=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE,related_name="edificacion")
+    creates=models.DateTimeField(auto_now_add=True)
+    
+    
+    
+    def __str__(self):
+        return self.direccion
+    
+
     
     
