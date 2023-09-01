503dbae99ac02bff782bd513d246ec8d8fa1bb08
analistadeti <jose.correa@zuana.com.co>
2023-09-01 17:34:06 -0500

    avance hotel 01 sep 2023

--- inmueble/inmuebleslist_app/admin.py
+++ inmueble/inmuebleslist_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from inmuebleslist_app.models import Edificacion,Empresa
+from inmuebleslist_app.models import Edificacion,Empresa,Comentario
 
 admin.site.register(Edificacion)         
-admin.site.register(Empresa)                                     
+admin.site.register(Empresa) 
+admin.site.register(Comentario)     
--- inmueble/inmuebleslist_app/models.py
+++ inmueble/inmuebleslist_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator , MaxValueValidator
 
 # Create your models here.
 
@@ -23,6 +24,17 @@ class Edificacion(models.Model):
     
     def __str__(self):
         return self.direccion
+
+class Comentario(models.Model):
+    calificacion = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    edificacion=models.ForeignKey(Edificacion, on_delete=models.CASCADE, related_name="comentarios")
+    texto = models.CharField(max_length=200,null=True)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.calificacion) + " " + self.edificacion.direccion
     
 
     
